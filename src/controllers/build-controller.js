5b94d9a51526aac8a3b46419d84b316dd86ccd93
Frederico Riachos <aluno20460@ipt.pt>
2020-10-05 01:27:22 +0100

    estrutura e testes com o postman num ficheiro json para test (books)

---
+++ src/controllers/build-controller.js
@@ -0,0 +1,41 @@
+const buildService = require('../services/build-service.js');
+
+exports.getBuilds=(req, res)=>{
+    buildService
+    .getBuilds()
+    .then(result=>res.json(result))
+    .catch(err=>res.status(500)
+    .send(err.message));
+};
+
+exports.getBuild=(req, res)=>{
+    buildService
+    .getBuild(req.params.id)
+    .then(result=>res.json(result))
+    .catch(err=>res.status(500)
+    .send(err.message));
+};
+
+exports.insertBuild=(req, res)=>{
+    buildService
+    .insertBuild(req.body)
+    .then(result=>res.json(result))
+    .catch(err=>res.status(500)
+    .send(err.message));
+};
+
+exports.updateBuild=(req, res)=>{
+    buildService
+    .getBuilds(req.params.id, req.body)
+    .then(result=>res.json(result))
+    .catch(err=>res.status(500)
+    .send(err.message));
+};
+
+exports.removeBuild=(req, res)=>{
+    buildService
+    .getBuilds(req.params.id)
+    .then(result=>res.json(result))
+    .catch(err=>res.status(500)
+    .send(err.message));
+};
